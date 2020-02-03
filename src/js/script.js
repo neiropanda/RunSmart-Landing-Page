7a8898b19a82ee505738017aeb43ebeb5ba4b420
Oleksandr <bekh.fa@gmail.com>
2020-02-04 01:30:54 +0200

    added slider and section with TABS

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+// $(document).ready(function () {
+//   $('.carousel__inner').slick({
+//     speed: 1000,
+//     // adaptiveHeight: true,
+//     prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.svg"></button>',
+//     nextArrow: '<button type="button" class="slick-next"><img src="../img/right.svg"></button>',
+//     responsive: [{
+//       breakpoint: 768,
+//       settings: {
+//         dots: true,
+//         arrows: false,
+//       }
+//     }]
+//   });
+// });
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: false,
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
